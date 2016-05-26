a4252bd6cd5f87f1f4d273eb83a2e4048669d3b0
Chris Uehlinger <chris.uehlinger@gmail.com>
2016-05-26 00:34:40 -0400

    Alphabetize exports

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 export {version} from "./build/package";
-export {default as geoStream} from "./src/stream";
 export {default as geoLength} from "./src/length";
+export {default as geoStream} from "./src/stream";
