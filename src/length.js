78b6d71d8084c563b65255220414ab0121737b96
Mike Bostock <mbostock@gmail.com>
2016-05-26 13:01:39 -0700

    Style improvements.

--- src/length.js
+++ src/length.js
@@ -2,22 +2,25 @@ import {deg2rad} from "./math";
 import noop from "./noop";
 import geoStream from "./stream";
 
-var lengthSum;
+var sum;
 
 var length = {
   sphere: noop,
   point: noop,
-  lineStart: lengthLineStart,
+  lineStart: lineStart,
   lineEnd: noop,
   polygonStart: noop,
   polygonEnd: noop
 };
 
-function lengthLineStart() {
-  var lambda0, sinPhi0, cosPhi0;
+function lineStart() {
+  var lambda0,
+      sinPhi0,
+      cosPhi0;
 
   length.point = function(lambda, phi) {
-    lambda0 = lambda * deg2rad, sinPhi0 = Math.sin(phi *= deg2rad), cosPhi0 = Math.cos(phi);
+    lambda *= deg2rad, phi *= deg2rad;
+    lambda0 = lambda, sinPhi0 = Math.sin(phi), cosPhi0 = Math.cos(phi);
     length.point = nextPoint;
   };
 
@@ -26,19 +29,23 @@ function lengthLineStart() {
   };
 
   function nextPoint(lambda, phi) {
-    var sinPhi = Math.sin(phi *= deg2rad),
+    lambda *= deg2rad, phi *= deg2rad;
+    var sinPhi = Math.sin(phi),
         cosPhi = Math.cos(phi),
-        t = Math.abs((lambda *= deg2rad) - lambda0),
-        cosDeltaLambda = Math.cos(t);
-    lengthSum += Math.atan2(
-      Math.sqrt((t = cosPhi * Math.sin(t)) * t + (t = cosPhi0 * sinPhi - sinPhi0 * cosPhi * cosDeltaLambda) * t), 
-      sinPhi0 * sinPhi + cosPhi0 * cosPhi * cosDeltaLambda);
+        delta = Math.abs(lambda - lambda0),
+        cosDelta = Math.cos(delta),
+        sinDelta = Math.sin(delta),
+        t;
+    sum += Math.atan2(
+      Math.sqrt((t = cosPhi * sinDelta) * t + (t = cosPhi0 * sinPhi - sinPhi0 * cosPhi * cosDelta) * t),
+      sinPhi0 * sinPhi + cosPhi0 * cosPhi * cosDelta
+    );
     lambda0 = lambda, sinPhi0 = sinPhi, cosPhi0 = cosPhi;
   }
 }
 
 export default function(object) {
-  lengthSum = 0;
+  sum = 0;
   geoStream(object, length);
-  return lengthSum;
+  return sum;
 }
