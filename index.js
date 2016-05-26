9d25d59247886235655bae850fb484ca45891102
Chris Uehlinger <chris.uehlinger@gmail.com>
2016-05-25 22:18:43 -0400

    Copy over graticule implementation from d3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export {version} from "./build/package";
+export {default as geoGraticule} from "./src/graticule";
 export {default as geoLength} from "./src/length";
 export {default as geoStream} from "./src/stream";
