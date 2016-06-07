8092bce1c0b5e016abb45452ae2df1a7a28ad968
Chris Uehlinger <chris.uehlinger@gmail.com>
2016-06-06 22:03:00 -0400

    Rewrite inDelta to properly handle arrays

--- test/inDelta.js
+++ test/inDelta.js
@@ -2,10 +2,20 @@ var tape = require("tape");
 
 tape.Test.prototype.inDelta = function(actual, expected, delta) {
   delta = delta || 1e-6;
-  this._assert(expected - delta < actual && actual < expected + delta, {
-    message: "should be in delta " + delta,
-    operator: "inDelta",
-    actual: actual,
-    expected: expected
-  });
+  this.assert(inDelta(actual, expected, delta), "expected " + actual + " to be in within *" + delta + "* of " + expected);
 };
+
+function inDelta(actual, expected, delta) {
+  return (Array.isArray(expected) ? inDeltaArray : inDeltaNumber)(actual, expected, delta);
+}
+
+function inDeltaArray(actual, expected, delta) {
+  var n = expected.length, i = -1;
+  if (actual.length !== n) return false;
+  while (++i < n) if (!inDelta(actual[i], expected[i], delta)) return false;
+  return true;
+}
+
+function inDeltaNumber(actual, expected, delta) {
+  return actual >= expected - delta && actual <= expected + delta;
+}
