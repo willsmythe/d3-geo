a4babbdb478aa692de23338343e3c0292d1141e9
Mike Bostock <mbostock@gmail.com>
2016-06-16 11:16:32 -0700

    One fewer closure.

--- src/length.js
+++ src/length.js
@@ -3,46 +3,48 @@ import {abs, atan2, cos, radians, sin, sqrt} from "./math";
 import noop from "./noop";
 import stream from "./stream";
 
-var lengthSum;
+var lengthSum,
+    lambda0,
+    sinPhi0,
+    cosPhi0;
 
 var lengthSink = {
   sphere: noop,
   point: noop,
-  lineStart: lineStart,
+  lineStart: lengthLineStart,
   lineEnd: noop,
   polygonStart: noop,
   polygonEnd: noop
 };
 
-function lineStart() {
-  var lambda0,
-      sinPhi0,
-      cosPhi0;
+function lengthLineStart() {
+  lengthSink.point = lengthPointFirst;
+  lengthSink.lineEnd = lengthLineEnd;
+}
 
-  lengthSink.point = function(lambda, phi) {
-    lambda *= radians, phi *= radians;
-    lambda0 = lambda, sinPhi0 = sin(phi), cosPhi0 = cos(phi);
-    lengthSink.point = nextPoint;
-  };
+function lengthLineEnd() {
+  lengthSink.point = lengthSink.lineEnd = noop;
+}
 
-  lengthSink.lineEnd = function() {
-    lengthSink.point = lengthSink.lineEnd = noop;
-  };
+function lengthPointFirst(lambda, phi) {
+  lambda *= radians, phi *= radians;
+  lambda0 = lambda, sinPhi0 = sin(phi), cosPhi0 = cos(phi);
+  lengthSink.point = lengthPoint;
+}
 
-  function nextPoint(lambda, phi) {
-    lambda *= radians, phi *= radians;
-    var sinPhi = sin(phi),
-        cosPhi = cos(phi),
-        delta = abs(lambda - lambda0),
-        cosDelta = cos(delta),
-        sinDelta = sin(delta),
-        t;
-    lengthSum.add(atan2(
-      sqrt((t = cosPhi * sinDelta) * t + (t = cosPhi0 * sinPhi - sinPhi0 * cosPhi * cosDelta) * t),
-      sinPhi0 * sinPhi + cosPhi0 * cosPhi * cosDelta
-    ));
-    lambda0 = lambda, sinPhi0 = sinPhi, cosPhi0 = cosPhi;
-  }
+function lengthPoint(lambda, phi) {
+  lambda *= radians, phi *= radians;
+  var sinPhi = sin(phi),
+      cosPhi = cos(phi),
+      delta = abs(lambda - lambda0),
+      cosDelta = cos(delta),
+      sinDelta = sin(delta),
+      t;
+  lengthSum.add(atan2(
+    sqrt((t = cosPhi * sinDelta) * t + (t = cosPhi0 * sinPhi - sinPhi0 * cosPhi * cosDelta) * t),
+    sinPhi0 * sinPhi + cosPhi0 * cosPhi * cosDelta
+  ));
+  lambda0 = lambda, sinPhi0 = sinPhi, cosPhi0 = cosPhi;
 }
 
 export default function(object) {
