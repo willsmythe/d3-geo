fe16b162053fad61aab85343b5300f4d06bc1c28
Mike Bostock <mbostock@gmail.com>
2016-06-17 20:34:21 -0700

    Simplify.

--- src/interpolate.js
+++ src/interpolate.js
@@ -1,7 +1,11 @@
 import {asin, atan2, cos, degrees, haversin, radians, sin, sqrt} from "./math";
 
-function interpolate(x0, y0, x1, y1) {
-  var cy0 = cos(y0),
+export default function(a, b) {
+  var x0 = a[0] * radians,
+      y0 = a[1] * radians,
+      x1 = b[0] * radians,
+      y1 = b[1] * radians,
+      cy0 = cos(y0),
       sy0 = sin(y0),
       cy1 = cos(y1),
       sy1 = sin(y1),
@@ -10,12 +14,11 @@ function interpolate(x0, y0, x1, y1) {
       kx1 = cy1 * cos(x1),
       ky1 = cy1 * sin(x1),
       d = 2 * asin(sqrt(haversin(y1 - y0) + cy0 * cy1 * haversin(x1 - x0))),
-      k = 1 / sin(d);
+      k = sin(d);
 
   var interpolate = d ? function(t) {
-    t *= d;
-    var B = sin(t) * k,
-        A = sin(d - t) * k,
+    var B = sin(t *= d) / k,
+        A = sin(d - t) / k,
         x = A * kx0 + B * kx1,
         y = A * ky0 + B * ky1,
         z = A * sy0 + B * sy1;
@@ -23,16 +26,11 @@ function interpolate(x0, y0, x1, y1) {
       atan2(y, x) * degrees,
       atan2(z, sqrt(x * x + y * y)) * degrees
     ];
-  } : function() { return [x0 * degrees, y0 * degrees]; };
+  } : function() {
+    return [x0 * degrees, y0 * degrees];
+  };
 
   interpolate.distance = d;
 
   return interpolate;
-}
-
-export default function(source, target) {
-  return interpolate(
-    source[0] * radians, source[1] * radians,
-    target[0] * radians, target[1] * radians
-  );
 }
