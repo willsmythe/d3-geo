39bc9bfae5c9c212347edfb221c5aa41d780c667
Noah <noah@noahveltman.com>
2016-07-13 14:54:41 -0400

    Updated tests

--- test/projection/fit-test.js
+++ test/projection/fit-test.js
@@ -2,151 +2,170 @@ var tape = require("tape"),
     topojson = require("topojson"),
     d3 = require("../../");
 
-var us = require("../data/us-10m.json"),
-    land = topojson.feature(us, us.objects.land);
+var usTopo = require("../data/us-10m.json"),
+    us = topojson.feature(usTopo, usTopo.objects.land),
+    worldTopo = require("../data/world-50m.json"),
+    world = topojson.feature(worldTopo, worldTopo.objects.land);
+
+// TODO: hemisphere, pole, clipExtent, custom projection
 
 require("../inDelta");
-
-tape("fit: USA Equirectangular", function(test){
+tape("fit: world Equirectangular", function(test){
 
   var projection = d3.geoEquirectangular();
 
-  projection.fit(land, [[50, 50],[950, 950]]);
+  projection.fit(world, [[50, 50], [950, 950]]);
 
-  test.inDelta(projection.scale(), 143.783157, 1e-6);
-  test.inDelta(projection.translate(), [498.876615, 611.701678], 1e-6);
+  test.inDelta(projection.scale(), 143.239449, 1e-6);
+  test.inDelta(projection.translate(), [500, 491.999512], 1e-6);
   test.end();
 
 });
 
-tape("fit: USA AzimuthalEqualArea", function(test){
+tape("fit: world AzimuthalEqualArea", function(test){
 
   var projection = d3.geoAzimuthalEqualArea();
 
-  projection.fit(land, [[50, 50],[950, 950]]);
+  projection.fit(world, [[50, 50], [950, 950]]);
 
-  test.inDelta(projection.scale(), 542.032665, 1e-6);
-  test.inDelta(projection.translate(), [854.206987, 1086.638566], 1e-6);
+  test.inDelta(projection.scale(), 228.357167, 1e-6);
+  test.inDelta(projection.translate(), [496.353437, 479.684335], 1e-6);
   test.end();
 
 });
 
-tape("fit: USA AzimuthalEquidistant", function(test){
+tape("fit: world AzimuthalEquidistant", function(test){
 
   var projection = d3.geoAzimuthalEquidistant();
 
-  projection.fit(land, [[50, 50],[950, 950]]);
+  projection.fit(world, [[50, 50], [950, 950]]);
 
-  test.inDelta(projection.scale(), 404.464479, 1e-6);
-  test.inDelta(projection.translate(), [861.570919, 1031.450355], 1e-6);
+  test.inDelta(projection.scale(), 153.559157, 1e-6);
+  test.inDelta(projection.translate(), [485.272655, 452.093361], 1e-6);
   test.end();
 
 });
 
-tape("fit: USA ConicConformal", function(test){
+tape("fit: world ConicConformal", function(test){
 
-  var projection = d3.geoConicConformal();
+  var projection = d3.geoConicConformal()
+      .clipAngle(30)
+      .parallels([30, 60])
+      .rotate([0, -45]);
 
-  projection.fit(land, [[50, 50],[950, 950]]);
+  projection.fit(world, [[50, 50], [950, 950]]);
 
-  test.inDelta(projection.scale(), 337.510571, 1e-6);
-  test.inDelta(projection.translate(), [581.872074, 930.761754], 1e-6);
+  test.inDelta(projection.scale(), 626.111017, 1e-6);
+  test.inDelta(projection.translate(), [444.395872, 410.223792], 1e-6);
   test.end();
 
 });
 
-tape("fit: USA ConicEqualArea", function(test){
+tape("fit: world ConicEqualArea", function(test){
 
   var projection = d3.geoConicEqualArea();
 
-  projection.fit(land, [[50, 50],[950, 950]]);
+  projection.fit(world, [[50, 50], [950, 950]]);
 
-  test.inDelta(projection.scale(), 291.198886, 1e-6);
-  test.inDelta(projection.translate(), [576.510061, 877.777053], 1e-6);
+  test.inDelta(projection.scale(), 145.862346, 1e-6);
+  test.inDelta(projection.translate(), [500, 591.911769], 1e-6);
   test.end();
 
 });
 
-tape("fit: USA ConicEquidistant", function(test){
+tape("fit: world ConicEquidistant", function(test){
 
   var projection = d3.geoConicEquidistant();
 
-  projection.fit(land, [[50, 50],[950, 950]]);
+  projection.fit(world, [[50, 50], [950, 950]]);
 
-  test.inDelta(projection.scale(), 311.156071, 1e-6);
-  test.inDelta(projection.translate(), [578.496843, 899.864338], 1e-6);
+  test.inDelta(projection.scale(), 123.085, 1e-6);
+  test.inDelta(projection.translate(), [500, 528.541415], 1e-6);
   test.end();
 
 });
 
-tape("fit: USA Equirectangular", function(test){
+tape("fit: world Equirectangular", function(test){
 
   var projection = d3.geoEquirectangular();
 
-  projection.fit(land, [[50, 50],[950, 950]]);
+  projection.fit(world, [[50, 50], [950, 950]]);
 
-  test.inDelta(projection.scale(), 143.783157, 1e-6);
-  test.inDelta(projection.translate(), [498.876615, 611.701678], 1e-6);
+  test.inDelta(projection.scale(), 143.239449, 1e-6);
+  test.inDelta(projection.translate(), [500, 491.999512], 1e-6);
   test.end();
 
 });
 
-tape("fit: USA Gnomonic", function(test){
+tape("fit: world Gnomonic", function(test){
 
-  var projection = d3.geoGnomonic().clipAngle(90);
+  var projection = d3.geoGnomonic()
+      .clipAngle(45);
 
-  projection.fit(land, [[50, 50],[950, 950]]);
+  projection.fit(world, [[50, 50], [950, 950]]);
 
-  test.inDelta(projection.scale(), 0, 1e-6);
-  test.inDelta(projection.translate(), [904.503764, 950], 1e-6);
+  test.inDelta(projection.scale(), 450.348236, 1e-6);
+  test.inDelta(projection.translate(), [500.115152, 556.52294], 1e-6);
   test.end();
 
 });
 
-tape("fit: USA Mercator", function(test){
+tape("fit: world Mercator", function(test){
 
   var projection = d3.geoMercator();
 
-  projection.fit(land, [[50, 50],[950, 950]]);
+  projection.fit(world, [[50, 50], [950, 950]]);
 
-  test.inDelta(projection.scale(), 143.783157, 1e-6);
-  test.inDelta(projection.translate(), [498.876615, 652.399043], 1e-6);
+  test.inDelta(projection.scale(), 143.239449, 1e-6);
+  test.inDelta(projection.translate(), [500, 481.549457], 1e-6);
   test.end();
 
 });
 
-tape("fit: USA Orthographic", function(test){
+tape("fit: world Orthographic", function(test){
 
   var projection = d3.geoOrthographic();
 
-  projection.fit(land, [[50, 50],[950, 950]]);
+  projection.fit(world, [[50, 50], [950, 950]]);
 
-  test.inDelta(projection.scale(), 2037.833598, 1e-6);
-  test.inDelta(projection.translate(), [2052.48573, 1568.919598], 1e-6);
+  test.inDelta(projection.scale(), 451.428643, 1e-6);
+  test.inDelta(projection.translate(), [503.769378, 498.61496], 1e-6);
   test.end();
 
 });
 
-tape("fit: USA Stereographic", function(test){
+tape("fit: world Stereographic", function(test){
 
   var projection = d3.geoStereographic();
 
-  projection.fit(land, [[50, 50],[950, 950]]);
+  projection.fit(world, [[50, 50], [950, 950]]);
 
-  test.inDelta(projection.scale(), 483.390791, 1e-6);
-  test.inDelta(projection.translate(), [751.728762, 1054.50814], 1e-6);
+  test.inDelta(projection.scale(), 162.934518, 1e-6);
+  test.inDelta(projection.translate(), [478.546849, 432.922426], 1e-6);
   test.end();
 
 });
 
-tape("fit: USA TransverseMercator", function(test){
+tape("fit: world TransverseMercator", function(test){
 
   var projection = d3.geoTransverseMercator();
 
-  projection.fit(land, [[50, 50],[950, 950]]);
+  projection.fit(world, [[50, 50], [950, 950]]);
 
-  test.inDelta(projection.scale(), 420.414716, 1e-6);
-  test.inDelta(projection.translate(), [786.851375, 1219.474867], 1e-6);
+  test.inDelta(projection.scale(), 143.239449, 1e-6);
+  test.inDelta(projection.translate(), [473.829753, 500], 1e-6);
+  test.end();
+
+});
+
+tape("fit: USA AlbersUsa", function(test){
+
+  var projection = d3.geoAlbersUsa();
+
+  projection.fit(us, [[50, 50], [950, 950]]);
+
+  test.inDelta(projection.scale(), 1152.889035, 1e-6);
+  test.inDelta(projection.translate(), [533.52541, 496.232028], 1e-6);
   test.end();
 
 });
