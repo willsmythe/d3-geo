1220a80df0e5cd717edca3a1b51264f73e8621e2
Noah <noah@noahveltman.com>
2016-07-14 11:58:11 -0400

    Saving clipExtent

--- src/projection/fit.js
+++ src/projection/fit.js
@@ -4,10 +4,16 @@ import boundsStream from "../path/bounds";
 function fit(project, extent, object) {
 
   var w = extent[1][0] - extent[0][0],
-      h = extent[1][1] - extent[0][1];
+      h = extent[1][1] - extent[0][1],
+      precision = project.precision(),
+      clip = project.clipExtent && project.clipExtent();
 
   project.scale(1)
       .translate([0, 0]);
+
+  if (clip !== undefined) {
+    project.clipExtent(null);
+  }
 
   geoStream(object, project.stream(boundsStream));
 
@@ -16,8 +22,13 @@ function fit(project, extent, object) {
       x = extent[0][0] + (w - s * (b[1][0] + b[0][0])) / 2,
       y = extent[0][1] + (h - s * (b[1][1] + b[0][1])) / 2;
 
+  if (clip !== undefined) {
+    project.clipExtent(clip);
+  }
+
   return project.scale(s)
-      .translate([x, y]);
+      .translate([x, y])
+      .precision(precision);
 };
 
 export function fitSize(project) {
