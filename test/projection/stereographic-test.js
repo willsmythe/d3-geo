641772bd2100583e62cc3856446a7ce834cb041f
Mike Bostock <mbostock@gmail.com>
2016-08-15 11:16:04 -0700

    Fix test.

--- test/projection/stereographic-test.js
+++ test/projection/stereographic-test.js
@@ -3,14 +3,12 @@ var tape = require("tape"),
 
 require("./projectionEqual");
 
-var pi = Math.PI;
-
 tape("stereographic(point) returns the expected result", function(test) {
   var stereographic = d3.geoStereographic().translate([0, 0]).scale(1);
   test.projectionEqual(stereographic, [  0,   0], [ 0,  0]);
   test.projectionEqual(stereographic, [-90,   0], [-1,  0]);
   test.projectionEqual(stereographic, [ 90,   0], [ 1,  0]);
-  test.projectionEqual(stereographic, [  0, -90], [ 0, -1]);
-  test.projectionEqual(stereographic, [  0,  90], [ 0,  1]);
+  test.projectionEqual(stereographic, [  0, -90], [ 0,  1]);
+  test.projectionEqual(stereographic, [  0,  90], [ 0, -1]);
   test.end();
 });
