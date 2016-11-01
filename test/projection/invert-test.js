c2a3d36542546b8f869410981ac01d30481ad401
Mike Bostock <mbostock@gmail.com>
2016-11-01 09:01:06 -0700

    Test albersUsa.invert.

--- test/projection/invert-test.js
+++ test/projection/invert-test.js
@@ -3,36 +3,42 @@ var tape = require("tape"),
 
 require("./projectionEqual");
 
-tape("projection.invert(projection(point)) returns the point", function(test) {
-  [
-    d3.geoAlbers(),
-    /* d3.geoAlbersUsa(), */
-    d3.geoAzimuthalEqualArea(),
-    d3.geoAzimuthalEquidistant(),
-    d3.geoConicConformal(),
-    d3.geoConicConformal().parallels([20,30]),
-    d3.geoConicConformal().parallels([30,30]),
-    d3.geoConicConformal().parallels([-35,-50]),
-    d3.geoConicEqualArea(),
-    d3.geoConicEqualArea().parallels([20,30]),
-    d3.geoConicEqualArea().parallels([-30,30]),
-    d3.geoConicEqualArea().parallels([-35,-50]), // https://github.com/d3/d3/issues/2707
-    d3.geoConicEquidistant(),
-    d3.geoConicEquidistant().parallels([20,30]),
-    d3.geoConicEquidistant().parallels([30,30]),
-    d3.geoConicEquidistant().parallels([-35,-50]),
-    d3.geoEquirectangular(),
-    d3.geoGnomonic(),
-    d3.geoMercator(),
-    d3.geoOrthographic(),
-    d3.geoStereographic(),
-    d3.geoTransverseMercator(),
-  ]
-  .forEach(function(projection) {
-     [ [0, 0], [30.3, 24.1], [ -10, 42 ], [ -2, -5 ] ]
-     .forEach(function(point) {
-         test.projectionEqual(projection, point, projection(point));
-     });
+[
+  d3.geoAlbers,
+  d3.geoAzimuthalEqualArea,
+  d3.geoAzimuthalEquidistant,
+  d3.geoConicConformal,
+  function conicConformal() { return d3.geoConicConformal().parallels([20, 30]); },
+  function conicConformal() { return d3.geoConicConformal().parallels([30, 30]); },
+  function conicConformal() { return d3.geoConicConformal().parallels([-35, -50]); },
+  d3.geoConicEqualArea,
+  function conicEqualArea() { return d3.geoConicEqualArea().parallels([20, 30]); },
+  function conicEqualArea() { return d3.geoConicEqualArea().parallels([-30, 30]); },
+  function conicEqualArea() { return d3.geoConicEqualArea().parallels([-35, -50]); },
+  d3.geoConicEquidistant,
+  function conicEquidistant() { return d3.geoConicEquidistant().parallels([20, 30]); },
+  function conicEquidistant() { return d3.geoConicEquidistant().parallels([30, 30]); },
+  function conicEquidistant() { return d3.geoConicEquidistant().parallels([-35, -50]); },
+  d3.geoEquirectangular,
+  d3.geoGnomonic,
+  d3.geoMercator,
+  d3.geoOrthographic,
+  d3.geoStereographic,
+  d3.geoTransverseMercator
+].forEach(function(factory) {
+  var name = factory.name, projection = factory();
+  tape(name + "(point) and " + name + ".invert(point) are symmetric", function(test) {
+    [[0, 0], [30.3, 24.1], [-10, 42], [-2, -5]].forEach(function(point) {
+      test.projectionEqual(projection, point, projection(point));
+    });
+    test.end();
+  });
+});
+
+tape("albersUsa(point) and albersUsa.invert(point) are symmetric", function(test) {
+  var projection = d3.geoAlbersUsa();
+  [[-122.4194, 37.7749], [-74.0059, 40.7128], [-149.9003, 61.2181], [-157.8583, 21.3069]].forEach(function(point) {
+    test.projectionEqual(projection, point, projection(point));
   });
   test.end();
 });
