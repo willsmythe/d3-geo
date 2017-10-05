01dfd54caba399063b0f06c74ff3a12be551280b
Mike Bostock <mbostock@gmail.com>
2017-10-05 09:20:54 -0700

    Don’t reuse fitExtent for fit{Width,Height}.

--- src/projection/fit.js
+++ src/projection/fit.js
@@ -1,31 +1,25 @@
 import {default as geoStream} from "../stream";
 import boundsStream from "../path/bounds";
 
-export function fitExtent(projection, extent, object) {
-  var w = extent[1][0] - extent[0][0],
-      h = extent[1][1] - extent[0][1],
-      clip = projection.clipExtent && projection.clipExtent();
-
-  projection
-      .scale(150)
-      .translate([0, 0]);
-
+function fit(projection, fitBounds, object) {
+  var clip = projection.clipExtent && projection.clipExtent();
+  projection.scale(150).translate([0, 0]);
   if (clip != null) projection.clipExtent(null);
-
   geoStream(object, projection.stream(boundsStream));
-
-  var b = boundsStream.result(),
-      k = Math.min(w / (b[1][0] - b[0][0]), h / (b[1][1] - b[0][1])),
-      ew = w !== Infinity ? w : (b[1][0] - b[0][0]) * k,
-      eh = h !== Infinity ? h : (b[1][1] - b[0][1]) * k,
-      x = +extent[0][0] + (ew - k * (b[1][0] + b[0][0])) / 2,
-      y = +extent[0][1] + (eh - k * (b[1][1] + b[0][1])) / 2;
-
+  fitBounds(boundsStream.result());
   if (clip != null) projection.clipExtent(clip);
+  return projection;
+}
 
-  return projection
-      .scale(k * 150)
-      .translate([x, y]);
+export function fitExtent(projection, extent, object) {
+  return fit(projection, function(b) {
+    var w = extent[1][0] - extent[0][0],
+        h = extent[1][1] - extent[0][1],
+        k = Math.min(w / (b[1][0] - b[0][0]), h / (b[1][1] - b[0][1])),
+        x = +extent[0][0] + (w - k * (b[1][0] + b[0][0])) / 2,
+        y = +extent[0][1] + (h - k * (b[1][1] + b[0][1])) / 2;
+    projection.scale(150 * k).translate([x, y]);
+  }, object);
 }
 
 export function fitSize(projection, size, object) {
@@ -33,9 +27,21 @@ export function fitSize(projection, size, object) {
 }
 
 export function fitWidth(projection, width, object) {
-  return fitExtent(projection, [[0, 0], [width, Infinity]], object);
+  return fit(projection, function(b) {
+    var w = +width,
+        k = w / (b[1][0] - b[0][0]),
+        x = (w - k * (b[1][0] + b[0][0])) / 2,
+        y = -k * b[0][1];
+    projection.scale(150 * k).translate([x, y]);
+  }, object);
 }
 
 export function fitHeight(projection, height, object) {
-  return fitExtent(projection, [[0, 0], [Infinity, height]], object);
+  return fit(projection, function(b) {
+    var h = +height,
+        k = h / (b[1][1] - b[0][1]),
+        x = -k * b[0][0],
+        y = (h - k * (b[1][1] + b[0][1])) / 2;
+    projection.scale(150 * k).translate([x, y]);
+  }, object);
 }
